40f944b75cb19970304536c92cb718e6b1e717ac
myCode2206 <rajatyt2206@gmail.com>
2024-03-11 14:02:44 +0530

    few changes

--- Frontend/src/Components/Login/Login.jsx
+++ Frontend/src/Components/Login/Login.jsx
@@ -42,125 +42,129 @@ const Login = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.forms}>
-        {isLogin ? (
-          <div className={styles.form}>
-            <span className={styles.title}>Login</span>
-            <form onSubmit={handleSubmit}>
-              <div className={styles.inputField}>
-                <input
-                  id="inp-email"
-                  type="text"
-                  placeholder="Enter your email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                />
-                <i className="uil uil-envelope icon"></i>
-              </div>
-              <div className={styles.inputField}>
-                <input
-                  type={showPassword ? "text" : "password"}
-                  id="inp-pass"
-                  className={styles.password}
-                  placeholder="Enter your password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  required
-                />
-                <i className="uil uil-lock icon"></i>
-                <i
-                  className={`uil ${showPassword ? "uil-eye" : "uil-eye-slash"} ${styles.showHidePw}`}
-                  onClick={togglePasswordVisibility}
-                ></i>
-              </div>
-              <div className={styles.checkboxText}>
-                <div className={styles.checkboxContent}>
-                  <input type="checkbox" id="logCheck" />
-                  <label htmlFor="logCheck" className={styles.text}>
-                    Remember me
-                  </label>
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.forms}>
+          {isLogin ? (
+            <div className={styles.form}>
+              <span className={styles.title}>Login</span>
+              <form onSubmit={handleSubmit}>
+                <div className={styles.inputField}>
+                  <input
+                    id="inp-email"
+                    type="text"
+                    placeholder="Enter your email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                  />
+                  <i className="uil uil-envelope icon"></i>
                 </div>
-                <a href="#" className={styles.text}>
-                  Forgot password?
-                </a>
-              </div>
-              <div className={styles.inputField}>
-                <button type="submit">Login</button>
-              </div>
-            </form>
-            <div className={styles.loginSignup}>
-              <span className={styles.text}>
-                Not a member?{" "}
-                <a className={styles.text} onClick={handleFormSwitch}>
-                  Signup Now
-                </a>
-              </span>
-            </div>
-          </div>
-        ) : (
-          <div className={styles.form}>
-            <span className={styles.title}>SignUp</span>
-            <form onSubmit={handleSubmit}>
-              <div className={styles.inputField}>
-                <input
-                  type="text"
-                  placeholder="Enter your name"
-                  name="username"
-                  value={formData.username}
-                  onChange={handleChange}
-                  required
-                />
-                <i className="uil uil-user"></i>
-              </div>
-              <div className={styles.inputField}>
-                <input
-                  type="text"
-                  placeholder="Enter your email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                />
-                <i className="uil uil-envelope icon"></i>
-              </div>
-              <div className={styles.inputField}>
-                <input
-                  type="password"
-                  className={styles.password}
-                  placeholder="Create a password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  required
-                />
-                <i className=" uil uil-lock icon"></i>
-              </div>
-              <div className={styles.checkboxText}>
-                <div className={styles.checkboxContent}>
-                  <input type="checkbox" id="termCon" />
-                  <label htmlFor="termCon" className={styles.text}>
-                    I accepted all terms and conditions
-                  </label>
+                <div className={styles.inputField}>
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    id="inp-pass"
+                    className={styles.password}
+                    placeholder="Enter your password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    required
+                  />
+                  <i className="uil uil-lock icon"></i>
+                  <i
+                    className={`uil ${
+                      showPassword ? "uil-eye" : "uil-eye-slash"
+                    } ${styles.showHidePw}`}
+                    onClick={togglePasswordVisibility}
+                  ></i>
                 </div>
+                <div className={styles.checkboxText}>
+                  <div className={styles.checkboxContent}>
+                    <input type="checkbox" id="logCheck" />
+                    <label htmlFor="logCheck" className={styles.text}>
+                      Remember me
+                    </label>
+                  </div>
+                  <a href="#" className={styles.text}>
+                    Forgot password?
+                  </a>
+                </div>
+                <div className={styles.inputField}>
+                  <button type="submit">Login</button>
+                </div>
+              </form>
+              <div className={styles.loginSignup}>
+                <span className={styles.text}>
+                  Not a member?{" "}
+                  <a className={styles.text} onClick={handleFormSwitch}>
+                    Signup Now
+                  </a>
+                </span>
               </div>
-              <div className={styles.inputField}>
-                <button type="submit">Signup</button>
-              </div>
-            </form>
-            <div className={styles.loginSignup}>
-              <span className={styles.text}>
-                Already have an account?{" "}
-                <a className={styles.text} onClick={handleFormSwitch}>
-                  Log In
-                </a>
-              </span>
             </div>
-          </div>
-        )}
+          ) : (
+            <div className={styles.form}>
+              <span className={styles.title}>SignUp</span>
+              <form onSubmit={handleSubmit}>
+                <div className={styles.inputField}>
+                  <input
+                    type="text"
+                    placeholder="Enter your name"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleChange}
+                    required
+                  />
+                  <i className="uil uil-user"></i>
+                </div>
+                <div className={styles.inputField}>
+                  <input
+                    type="text"
+                    placeholder="Enter your email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                  />
+                  <i className="uil uil-envelope icon"></i>
+                </div>
+                <div className={styles.inputField}>
+                  <input
+                    type="password"
+                    className={styles.password}
+                    placeholder="Create a password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    required
+                  />
+                  <i className=" uil uil-lock icon"></i>
+                </div>
+                <div className={styles.checkboxText}>
+                  <div className={styles.checkboxContent}>
+                    <input type="checkbox" id="termCon" />
+                    <label htmlFor="termCon" className={styles.text}>
+                      I accepted all terms and conditions
+                    </label>
+                  </div>
+                </div>
+                <div className={styles.inputField}>
+                  <button type="submit">Signup</button>
+                </div>
+              </form>
+              <div className={styles.loginSignup}>
+                <span className={styles.text}>
+                  Already have an account?{" "}
+                  <a className={styles.text} onClick={handleFormSwitch}>
+                    Log In
+                  </a>
+                </span>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
--- Frontend/src/Components/Navbar/MyNav.jsx
+++ Frontend/src/Components/Navbar/MyNav.jsx
@@ -36,7 +36,7 @@ function MyNav() {
                   style={{}}
                 >
                   <Nav.Link href="/">Home</Nav.Link>
-                  <Nav.Link href="/about">About</Nav.Link>
+                  <Nav.Link href="/blogs">Blogs</Nav.Link>
                   <Nav.Link href="/recipe">Recipe</Nav.Link>
                   <Nav.Link href="/contactus">Contact Us</Nav.Link>
                   {/* <NavDropdown
