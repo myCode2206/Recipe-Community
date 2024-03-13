bdcc43a06560f9fe42861fb1d5a8b5fdeb658333
myCode2206 <rajatyt2206@gmail.com>
2024-03-13 22:59:47 +0530

    fixed card data overflow

--- Backend/index.js
+++ Backend/index.js
@@ -1,36 +1,35 @@
-const express=require('express');
-const app=express();
-const mongoose=require('mongoose');
-const cors=require('cors');
-// const seedDB = require('./seed')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+// const seedDB = require("./seed");
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+mongoose
+  .connect("mongodb://localhost:27017/recipeeee")
+  .then(() => console.log("Connection Open!"))
+  .catch((err) => console.log(err));
 
-mongoose.connect('mongodb://127.0.0.1:27017/recipeeee')
-    .then(() => console.log('Connection Open!'))
-    .catch((err) => console.log(err));
-
-app.use(cors({
-    origin:['http://localhost:5173']
-  }));
-
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 
 //all routes
-const BlogRoutes=require('./Routes/BlogRoutes');
-const NormalRoutes=require('./Routes/NormalRoutes');
-const RecipeRoutes=require('./Routes/RecipeRoutes');
-const UserRoutes=require('./Routes/UserRoutes');
+const BlogRoutes = require("./Routes/BlogRoutes");
+const NormalRoutes = require("./Routes/NormalRoutes");
+const RecipeRoutes = require("./Routes/RecipeRoutes");
+const UserRoutes = require("./Routes/UserRoutes");
 app.use(BlogRoutes);
 app.use(NormalRoutes);
 app.use(RecipeRoutes);
 app.use(UserRoutes);
 
-
 // seedDB();
 
-
-app.listen('5000',()=>{
-    console.log("conected to port 5000");
-})
+app.listen("5000", () => {
+  console.log("conected to port 5000");
+});
--- Frontend/src/Components/Cards/MyCard.jsx
+++ Frontend/src/Components/Cards/MyCard.jsx
@@ -2,17 +2,13 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 function MyCard(props) {
-
-  const navigate=useNavigate();
-  const  showRecipeHandler = (id)=>
-  {
-    console.log("bhai merko pt dba")
-    navigate(`/recipe/${id}`)
-  }
-  
+  const navigate = useNavigate();
+  const showRecipeHandler = (id) => {
+    console.log("bhai merko pt dba");
+    navigate(`/recipe/${id}`);
+  };
 
   return (
     <Card
@@ -31,22 +27,15 @@ function MyCard(props) {
       />
       <Card.Body>
         <Card.Title>{props.item.title}</Card.Title>
-        <Card.Text>
-        {props.item.body}
-        </Card.Text>
-        <Button onClick={()=>showRecipeHandler(props.item._id)}
-          style={{ 
+        <Card.Text>{props.item.body.substring(0, 100)}...</Card.Text>
+        <Button
+          onClick={() => showRecipeHandler(props.item._id)}
+          style={{
             backgroundColor: "#ffee32",
             color: "black",
           }}
           variant="primary"
         >
-          {/* <Link
-            style={{ textDecoration: "none", color: "black" }}
-            to="/showRecipe"
-          >
-            Read More
-          </Link> */}
           Read More
         </Button>
       </Card.Body>
