c897861b36bc2c59e5b1624baf375f9824bfb835
Aditya Chauhan <adityachauhan.ac2307@gmail.com>
2024-03-14 15:32:49 +0530

    nav update 2.0

--- Frontend/src/Components/Navbar/MyNav.jsx
+++ Frontend/src/Components/Navbar/MyNav.jsx
@@ -18,7 +18,7 @@ function MyNav() {
           className="mb-3"
         >
           <Container fluid>
-            <Navbar.Brand href="#">Reciepe</Navbar.Brand>
+            <Navbar.Brand href="#">Recipe</Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
