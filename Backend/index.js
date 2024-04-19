1e1cb23f10aed29e610d46e547d093110f3ade22
Mukul <mukul1singh23@gmail.com>
2024-04-19 11:43:40 +0530

    changed database

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,21 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const seedDB = require("./seed");
+const dotenv=require('dotenv').config()
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+
+const mongourl = process.env.MONGO_URL;
+const port = process.env.PORT;
+
+
+
 mongoose
-  .connect("mongodb://127.0.0.1:27017/recipeeee")
+  .connect(mongourl)
   .then(() => console.log("Connection Open!"))
   .catch((err) => console.log(err));
 
@@ -30,6 +39,6 @@ app.use(UserRoutes);
 
 // seedDB();
 
-app.listen("5000", () => {
+app.listen(port, () => {
   console.log("conected to port 5000");
 });
