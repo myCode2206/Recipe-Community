39aa108063d101118d77d6d7921bd318fdffd397
myCode2206 <rajatyt2206@gmail.com>
2024-04-21 21:14:46 +0530

    form-page-added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,24 +6,24 @@ import "./App.css";
 import ShowRecipe from "./pages/Show Recipe/ShowRecipe";
 import { MyBlogs } from "./pages/Blogs/MyBlogs";
 import { ShowBlogs } from "./pages/Show Blogs/ShowBlogs";
-import  { Contact } from "./pages/contactUs/Contact";
+import { Contact } from "./pages/contactUs/Contact";
 import Home from "./pages/Home/Home";
-import { RecoilRoot } from "recoil";
+import AddRecipe from "./Components/Form/AddRecipe";
 const App = () => {
   return (
     <>
       <Router>
-    
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/recipe" element={<Recipe />} />
+          <Route path="/recipe/:id" element={<ShowRecipe />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/showRecipe/:id" element={<ShowRecipe />} />
+          <Route path="/showRecipe" element={<ShowRecipe />} />
           <Route path="/blogs" element={<MyBlogs />} />
-          <Route path="/showBlogs/:id" element={<ShowBlogs />} />
+          <Route path="/showBlogs" element={<ShowBlogs />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/add" element={<AddRecipe />} />
         </Routes>
-   
       </Router>
     </>
   );
--- Frontend/src/pages/Recipe/Recipe.jsx
+++ Frontend/src/pages/Recipe/Recipe.jsx
@@ -1,11 +1,18 @@
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import MyNav from "../../Components/Navbar/MyNav";
 import Footer from "../../Components/footer/Footer";
 import MyCard from "../../Components/Cards/MyCard";
 import SearchForm from "../../Components/Search Bar/SearchForm";
 import axios from "axios";
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 const Recipe = () => {
+  const navigate = useNavigate();
+
+  function Redirect() {
+    navigate("/add");
+  }
   //data fetch from api to get the content of cards
 
   const [recipes, setrecipe] = useState([]);
@@ -34,6 +41,32 @@ const Recipe = () => {
       <MyNav />
 
       <div style={{ textAlign: "center", marginTop: "30px" }}>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "end",
+            paddingRight: "20px",
+            marginBottom: "5px",
+            alignContent: "center",
+          }}
+        >
+          <button
+            style={{ right: "0" }}
+            className="btn btn-primary"
+            onClick={Redirect}
+          >
+            Add
+            <IoIosAddCircleOutline
+              color="#ffff"
+              style={{
+                marginLeft: "5px",
+                fontSize: "20px",
+                paddingBottom: "2px",
+              }}
+            />
+          </button>
+        </div>
         <h1>Discover Flavorful Creations</h1>
       </div>
       <div
@@ -51,7 +84,6 @@ const Recipe = () => {
         {recipes.map((recipe) => {
           return <MyCard key={recipe._id} item={recipe} />;
         })}
-        
       </div>
 
       <Footer />
