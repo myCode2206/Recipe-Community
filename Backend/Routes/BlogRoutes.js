72c8c09a2a911e514cb3173c5dcc268f65ea6dab
myCode2206 <rajatyt2206@gmail.com>
2024-04-24 11:05:21 +0530

    blog routes updated

--- Backend/Routes/BlogRoutes.js
+++ Backend/Routes/BlogRoutes.js
@@ -1,102 +1,95 @@
-const express=require('express')
-const router=express.Router();
-const MyBlog=require('../Models/blog')
-const BlogComment = require('../Models/blogComment');
+const express = require("express");
+const router = express.Router();
+const MyBlog = require("../Models/blog");
+const BlogComment = require("../Models/blogComment");
 
-router.get('/blog',async(req,res)=>{
-    try{
-    const blog= await MyBlog.find();
+router.get("/blog", async (req, res) => {
+  try {
+    const blog = await MyBlog.find();
     console.log("sare blogs aa jynge");
     res.status(200).json(blog);
-    }
-    catch (e) {
-        res.status(400).json({ msg: 'Something Went Wrong!!!' });
-    }
-})
+  } catch (e) {
+    res.status(400).json({ msg: "Something Went Wrong!!!" });
+  }
+});
 
-router.post('/blog/add', async(req,res)=>{
-    try{
-        const {title , image, body}=req.body;
-        const author="mukul";
-        const newblog = await MyBlog.create({title,image,body,author});
-        res.status(201).json({_id :newblog._id});
-        
-    }
-    catch (e) {
-        res.status(400).json({ msg: 'Something Went Wrong!!!' });
-    }
-    
-})
+router.post("/blog/add", async (req, res) => {
+  try {
+    const { blogName, img, desc } = req.body.formData;
+    const author = "mukul";
+    const newblog = await MyBlog.create({
+      title: blogName,
+      image: img,
+      body: desc,
+      author,
+    });
+    res.status(201).json({ _id: newblog._id });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ msg: "Something Went Wrong!!!" });
+  }
+});
 
-router.get('/blog/:id', async(req,res)=>{
-    try{
-        const {id} = req.params;
-        const blog= await MyBlog.findById(id)
-        // console.log("sare blogs aa jynge");
-        res.status(200).json(blog);
-    }
-    catch(e){
-        res.status(400).json({ msg: 'Something Went Wrong!!!' });
-    }
+router.get("/blog/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const blog = await MyBlog.findById(id);
+    res.status(200).json(blog);
+  } catch (e) {
+    res.status(400).json({ msg: "Something Went Wrong!!!" });
+  }
+});
 
-    
-})
+router.get("/blog/:id/updateblog", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const blog = await MyBlog.findById(id);
+    console.log("sare blogs aa jynge");
+    res.status(200).json(blog);
+  } catch (e) {
+    res.status(400).json({ msg: "somthing went wrong" });
+  }
+});
+router.post("/blog/:id/updateblog", (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, image, body } = req.body();
+    const blog = MyBlog.findByIdAndUpdate(id, {
+      title: title,
+      image: image,
+      body: body,
+    });
+    console.log("sare blogs aa jynge");
+    res.status(200).json(blog);
+  } catch (e) {
+    res.status(400).json({ msg: "somthing went wrong" });
+  }
+});
 
-router.get('/blog/:id/updateblog', async(req,res)=>{
-    try{
-        const {id} = req.params;
-        const blog= await MyBlog.findById(id);
-        console.log("sare blogs aa jynge");
-        res.status(200).json(blog);
-    }
-    catch(e){
-        res.status(400).json({msg:'somthing went wrong'});
-    }
-    
-})
-router.post('/blog/:id/updateblog',(req,res)=>{
-    try{
-        const {id} = req.params;
-        const {title,image,body}=req.body();
-        const blog=MyBlog.findByIdAndUpdate(id,{title:title ,image:image , body:body});
-        console.log("sare blogs aa jynge");
-        res.status(200).json(blog);
-    }
-    catch(e){
-        res.status(400).json({msg:'somthing went wrong'});
-    }
-})
+router.post("/blog/:id/comment", async (req, res) => {
+  try {
+    const { id } = req.params;
+    // console.log(id);
+    const blog = await MyBlog.findById(id);
+    // console.log(blog)
+    //
+    const { comment } = req.body;
+    console.log(req.body);
 
+    const newComment = await BlogComment.create({
+      comment,
+      user_id: "1",
+      username: "mukul",
+    });
 
+    // console.log(newComment)
 
-router.post('/blog/:id/comment', async (req,res)=>{
-    try{
+    blog.comment.push(newComment);
 
-        
-        const {id} = req.params;
-        // console.log(id);
-        const blog = await MyBlog.findById(id);
-        // console.log(blog)
-        // 
-        const {comment} = req.body;
-        console.log(req.body)
+    res.status(201).json({ msg: "comment added sucess" });
+  } catch (e) {
+    res.status(400).json({ msg: "error in adding comments" });
+  }
+});
 
-        const newComment = await BlogComment.create({comment,user_id:"1",username:"mukul"});
-
-        // console.log(newComment)
-
-        blog.comment.push(newComment);
-
-        res.status(201).json({msg:'comment added sucess'})
-
-    }
-    catch(e)
-    {
-        res.status(400).json({msg:'error in adding comments'})
-    }
-})
-
-
-
-
-module.exports= router;
+module.exports = router;
